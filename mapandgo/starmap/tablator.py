c91b90c7b86ce1b5c259fb41764b58a0a7b7c4c4
Anna Alemany <annalemany@gmail.com>
2018-07-19 13:10:47 +0200

    Update tablator.py

--- mapandgo/starmap/tablator.py
+++ mapandgo/starmap/tablator.py
@@ -11,7 +11,7 @@ try:
 except:
     sys.exit('Please, give input bamfile')
 
-if not os.path.isfile(inputSamFile):
+if not os.path.isfile(inputBamFile):
     sys.exit('bamfile not found')
 
 bamfile = pysam.AlignmentFile(inputBamFile, 'rb')
