171e6dc2476bcd56b49fdbbea3380c95289b95eb
Anna Alemany <annalemany@gmail.com>
2018-07-19 12:57:38 +0200

    Update submit_map.sh

--- mapandgo/starmap/submit_map.sh
+++ mapandgo/starmap/submit_map.sh
@@ -4,7 +4,7 @@ Dt=20:30:00
 Dmem=50G
 threads=1
 
-if [ $# -ne 5 ]
+if [ $# -ne 6 ]
 then
     echo "Please, give in this order the following inputs"
     echo "1) library name (name until 1_R*_001.fastq.gz)"
@@ -12,10 +12,11 @@ then
     echo "3) protocol [celseq1, celseq2, n]"
     echo "4) trim cbc.fastq file [y, n]"
     echo "5) reference folder [mouse, path, n]"
+    echo "6) create count tables [y, n]"
     exit
 fi
 
 fq=$1
 outfq=${fq%_*_S*_L*}
 
-echo "/hpc/hub_oudenaarden/aalemany/bin/map/starmap/map.sh $1 $2 $3 $4 $5" | qsub -cwd -N star-${outfq} -o star-${outfq}.out -e star-${outfq}.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem} -V
+echo "/hpc/hub_oudenaarden/aalemany/bin/map/starmap/map.sh $1 $2 $3 $4 $5 $6" | qsub -cwd -N star-${outfq} -o star-${outfq}.out -e star-${outfq}.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem} -V
