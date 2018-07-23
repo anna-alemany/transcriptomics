e7942326eed15052375cb9e7242c85dcc4a15839
Anna Alemany <annalemany@gmail.com>
2018-07-23 10:04:14 +0200

    Update bam2bed.sh

--- mapandgo/map/bam2bed.sh
+++ mapandgo/map/bam2bed.sh
@@ -6,9 +6,11 @@ if [ $# -n 1 ]
 then
     echo "Please, give:"
     echo "1) input bam file"
+    echo "2) output file"
     exit
 fi
 
-${path2bedtools}/bin/bamToBed -i $1 -split | awk -F '\t|:' '{col=NF; cell=col-2; umi=col-4; print $0"\t"$cell"\t"$umi}' > ${1%.bam}.bed
+${path2bedtools}/bin/bamToBed -i $1 -split | awk -F '\t|:' '{col=NF; cell=col-2; umi=col-4; print $0"\t"$cell"\t"$umi}' > $2
+
 
 
