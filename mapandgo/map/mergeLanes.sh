bc903df7c945b42c19adf13c8fbdfd5786a84f4c
Anna Alemany <annalemany@gmail.com>
2018-07-24 14:37:54 +0200

    Update mergeLanes.sh

--- mapandgo/map/mergeLanes.sh
+++ mapandgo/map/mergeLanes.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "Please, give root to fastq files"
+  echo "Please, give root for input and output fastq files"
   exit
 fi
 
 fq=$1
+outfq=$2
 zcat ${fq}*R1* > ${outfq}_R1.fastq
 zcat ${fq}*R2* > ${outfq}_R2.fastq
 gzip ${outfq}_R1.fastq
