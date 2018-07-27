c2e729250054b274a55267532a4084288c1a5aba
Anna Alemany <annalemany@gmail.com>
2018-07-27 16:43:14 +0200

    Update map.sh

--- mapandgo/map/map.sh
+++ mapandgo/map/map.sh
@@ -1,111 +1,31 @@
 #!/bin/bash
 
-#### Paths to scripts ####
-path2scripts=/hpc/hub_oudenaarden/aalemany/bin/mapandgo
-
-#### read input parameters ####
-
-if [ $# -ne 8 ]
+if [ $# -ne 1 ]
 then
-    echo "Please, give the following inputs in this order"
-    echo "1) library name (name until 1_R*_001.fastq.gz)"
-    echo "2) output root name (no extensions)"
-    echo "3) pool lanes [y, n]"
-    echo "4) protocol: celseq1, celseq2, scscar, n [to skip concatenator step]"
-    echo "5) trim cbc.fastq.gz file [y, n]"
-    echo "6) mapping software [bwa, star]" 
-    echo "7) reference genome [mouse, human, elegans, briggsae, zebrafish, GFP, full path for others, n for non (skip mapping)]"
-    echo "8) create count tables [y, n]"
+    echo "Please, give input root file"
     exit
 fi
 
-fq=$1
-outfq=$2
-pool=$3
-protocol=$4
-trim=$5
-soft=$6
-ref=$7
-count=$8
+p2s=/hpc/hub_oudenaarden/aalemany/bin/mapandgo2
 
-#### pool lanes ####
-if [ $pool == 'y' ]
-then
-    ${path2scripts}/mergeLanes.sh $fq $outfq
-elif [ $pool == 'n' ]
-then
-    echo "skip pooling lanes"
-else
-    echo 'Pool lanes [y/n] not specified'
-fi
 
-#### extract cell specific barcode and umi ####
-if [ $protocol =! 'n' ]
-then
-    ${path2scripts}/extractBC.sh ${outfq} ${protocol}
-    gzip ${outfq}_cbc.fastq
-elif [ $protocol == 'n' ]
-then
-    echo "skip concatenation to create cbc.fastq file"
-else
-    echo 'Protocol [celseq1, celseq2, scscar, n] not specified'
-fi
+in=$1
+out=${in%_*_S*_L*}
 
-#### trim low quality bases ####
-if [ $trim == 'y' ]
-then
-    file2trim=${outfq}_cbc.fastq.gz
-    if [ ! -f ${file2trim} ]
-    then
-        file2trim=${outfq}_cbc.fastq
-        if [ ! -f ${file2trim} ]
-            then
-            echo "file to trim (fastq or fastq.gz) not found"
-            exit
-        fi        
-    fi
-    ${path2scripts}/trim.sh ${file2trim}
-elif [ $trim == 'n' ]
-then
-    echo "skip trimming"
-else
-    echo "trim [y, n] not specified"
-    exit
-fi
+# merge data
+${p2s}/mergeLanes.sh $in $out
 
-#### map ####
-file2map=${outfq}_cbc_trimmed.fq.gz
-if [ ! -f ${file2map} ]
-then
-    file2map=${outfq}_cbc_trimmed.fq
-    if [ ! -f ${file2map} ]
-    then
-        echo "file to map _cbc_trimmed.fq or _cbc_trimmed.fq.gz not found"
-        exit
-    fi
-fi
+# extract barcodes
+${p2s}/extractBC.sh $out celseq2
 
-if [ $ref != 'n' ]
-then
-    if [ $soft == "bwa" ]
-    then
-        ${path2scripts}/mapbwa.sh ${file2map} ${outfq}_bwa $ref
-    elif [ $soft == 'star' ]
-    then
-        ${path2scripts}/mapstar.sh ${file2map} ${outfq}_star $ref
-    fi
-else
-    echo "skip mapping"
-fi
+# trim
+${p2s}/trim.sh ${out}_cbc.fastq.gz
 
-#### Produce count tables ####
-if [ $count == 'y' ]
-then
-    if [ $soft == 'bwa' ] 
-    then
-        ${path2scripts}/createCountTables.sh ${outfq}_bwa.bam
-    elif [ $soft == 'star' ] 
-    then
-        ${path2scripts}/createCountTables.sh ${outfq}_starAligned.toTranscriptome.out.bam
-    fi
-fi
+# map with bwa
+${p2s}/mapbwa.sh ${out}_cbc_trimmed.fq.gz ${out}_cbc_trimmed_bwa mouse
+
+# map with star
+${p2s}/mapstar.sh ${out}_cbc_trimmed.fq.gz ${out}_cbc_trimmed_star mouse
+
+# create cout tables from bwa map
+python ${p2s}/tablator_bwa.py ${out}_cbc_trimmed_bwa.bam
