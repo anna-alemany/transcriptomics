364dce801fd98e012a75a10e8a30ed406cd4f0e1
Anna Alemany <annalemany@gmail.com>
2018-08-13 15:28:13 +0200

    Update submit_map.sh

--- mapandgo/map/submit_map.sh
+++ mapandgo/map/submit_map.sh
@@ -9,7 +9,8 @@ if [ $# -ne 1 ]
 then
     echo "Please, give"
     echo "1) root input fastq lanes"
+    echo "2) protocol (celseq1, celseq2, nla)"
     exit
 fi
 
-echo "/hpc/hub_oudenaarden/aalemany/bin/mapandgo2/map.sh $1" | qsub -cwd -N map-$1 -o map-${1}.out -e map-${1}.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem}
+echo "/hpc/hub_oudenaarden/aalemany/bin/mapandgo2/map.sh $1 $2" | qsub -cwd -N map-$1 -o map-${1}.out -e map-${1}.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem}
