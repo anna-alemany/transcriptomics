17ded16f1f2ed5c2ccc56bedb0d7b279cc62130f
Anna Alemany <annalemany@gmail.com>
2018-08-16 16:27:20 +0200

    Update getIntronsExons.sh

--- RNAvelocity/getIntronsExons.sh
+++ RNAvelocity/getIntronsExons.sh
@@ -15,33 +15,35 @@ p2s=/hpc/hub_oudenaarden/bdebarbanson/bin/samtools-1.3.1
 intronbed=$2
 exonbed=$3
 
-#${p2b}/bamToBed -i $1 -split > ${4}_bam2bed.bed
-${p2b}/bedtools intersect -a ${4}_bam2bed.bed -b ${intronbed} -wb | awk '{if (($6==$10) && ($5==255)) print $1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$NF}' | uniq > ${4}_intron1.bed
+${p2b}/bamToBed -i $1 -split > ${4}_bam2bed.bed
+${p2b}/bedtools intersect -a ${4}_bam2bed.bed -b ${intronbed} -wb | awk '{if (($6==$10) && ($5==255)) print $1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$NF}' | uniq > ${4}_intron1.bed &
 ${p2b}/bedtools intersect -a ${4}_bam2bed.bed -b ${exonbed} -wb | awk '{if (($6==$10) && ($5==255)) print $1"\t"$2"\t"$3"\t"$4"\t"$6"\t"$NF}' | uniq > ${4}_exon1.bed
+wait
 
-chroms=$(${p2s}/samtools view -H SvdB12e2-10-Tomo2-4dAA-8umAP-mGstrlds_cbc_trimmed_starAligned.sortedByCoord.out.bam | awk -F 'SN:|\t' '{print $3}')
+chroms=$(${p2s}/samtools view -H $1 | awk -F 'SN:|\t' '{print $3}')
+
+if [ -f ${4}_intron.bed ]
+then
+    rm ${4}_intron.bed
+fi
+if [ -f ${4}_exon.bed ]
+then
+    rm ${4}_exon.bed
+fi
 
 for i in $(echo $chroms | awk '{for (i=1; i<=NF; i++) print $i}')
 do
-    awk -v i=$i '{if ($1==i) print $0}' ${4}_intron1.bed > ${4}_TMP_intron1_${i}.bed
+    awk -v i=$i '{if ($1==i) print $0}' ${4}_intron1.bed > ${4}_TMP_intron1_${i}.bed &
     awk -v i=$i '{if ($1==i) print $0}' ${4}_exon1.bed > ${4}_TMP_exon1_${i}.bed
+    wait
+
+    ${p2b}/bedtools intersect -a ${4}_TMP_intron1_${i}.bed -b ${4}_TMP_exon1_${i}.bed -v >> ${4}_intron.bed &
+    ${p2b}/bedtools intersect -a ${4}_TMP_exon1_${i}.bed -b ${4}_TMP_intron1_${i}.bed -v >> ${4}_exon.bed
+    wait
+
+    rm  ${4}_TMP_exon1_${i}.bed ${4}_TMP_intron1_${i}.bed
 done
 
-for i in $(echo $chroms | awk '{for (i=1; i<=NF; i++) print $i}')
-do
-    ${p2b}/bedtools intersect -a ${4}_TMP_intron1_${i}.bed -b ${4}_TMP_exon1_${i}.bed -v > ${4}_intron_TMP_${i}.bed &
-done
-wait
+#rm ${4}_intron1.bed ${4}_exon1.bed
 
-for i in $(echo $chroms | awk '{for (i=1; i<=NF; i++) print $i}')
-do
-    ${p2b}/bedtools intersect -a ${4}_TMP_exon1_${i}.bed -b ${4}_TMP_intron1_${i}.bed -v > ${4}_exon_TMP_${i}.bed &
-done
-wait
-
-#${p2b}/bedtools intersect -a ${4}_intron1.bed -b ${4}_exon1.bed -v > ${4}_introns.bed
-#${p2b}/bedtools intersect -a ${4}_exon1.bed -b ${4}_intron1.bed -v > ${4}_exons.bed
-
-#rm ${4}_bam2bed.bed ${4}_intron1.bed ${4}_exon1.bed
-
-python /hpc/hub_oudenaarden/aalemany/bin/RNAvelocity/countExonsIntrons.py ${4}_introns.bed ${4}_exons.bed $4
+python /hpc/hub_oudenaarden/aalemany/bin/RNAvelocity/countExonsIntrons.py ${4}_intron.bed ${4}_exon.bed $4
