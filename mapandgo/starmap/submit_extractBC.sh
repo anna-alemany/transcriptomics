44a4a718ead637e9a7c6017e16fc9bac1f25abec
Anna Alemany <annalemany@gmail.com>
2019-01-30 13:52:46 +0100

    Create submit_extractBC.sh

---
+++ mapandgo/starmap/submit_extractBC.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+email=a.alemany@hubrecht.eu
+Dt=15:00:00
+Dmem=10G
+threads=1
+
+if [ $# -ne 2 ]
+then
+    echo "Please, give these following inputs:"
+    echo "1) input root to fastq files"
+    echo "2) protocol [celseq1, celseq2, scscar, nla]"
+    exit
+fi
+
+infq=$1
+protocol=$2
+
+echo "/hpc/hub_oudenaarden/aalemany/bin/mapandgo2/extractBC.sh $infq $protocol" | qsub -V -cwd -N bc-${infq} -o bc-${infq}.out -e bc-${infq}.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem}
