0d4a104f25f5729d8e883b723f1b8856f7bc5529
Anna Alemany <annalemany@gmail.com>
2019-01-30 13:53:17 +0100

    Create extractBC.sh

---
+++ mapandgo/starmap/extractBC.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+path2scripts=/hpc/hub_oudenaarden/aalemany/bin/mapandgo2
+
+if [ $# -ne 2 ]
+then
+    echo "Please, give: "
+    echo "1) input root to fastq files"
+    echo "2) protocol [celseq1, celseq2, scscar, nla]"
+    exit
+fi
+
+outfq=$1
+protocol=$2
+
+if [ $protocol == 'celseq1' ]
+then
+    python3 ${path2scripts}/concatenator.py --fqf ${outfq} --cbcfile ${path2scripts}/bc_celseq1.tsv --cbchd 0 --lenumi 4
+    gzip ${outfq}_cbc.fastq
+elif [ $protocol == 'celseq2' ]
+then
+    python3 ${path2scripts}/concatenator.py --fqf ${outfq} --cbcfile ${path2scripts}/bc_celseq2.tsv --cbchd 0 --lenumi 6 --umifirst
+    gzip ${outfq}_cbc.fastq
+elif [ $protocol == 'scscar' ]
+then
+    python3 ${path2scripts}/concatenator.py --fqf ${outfq} --cbcfile ${path2scripts}/bc_scarsc.tsv --cbchd 0 --lenumi 3 --umifirst
+    gzip ${outfq}_cbc.fastq
+elif [ $protocol == 'nla' ]
+then
+    python3 ${path2scripts}/concatenator.py --fqf ${outfq} --cbcfile ${path2scripts}/bc_nla.tsv --cbchd 0 --lenumi 3 --umifirst
+    gzip ${outfq}_cbc.fastq
+else
+    echo "unknown protocol [celseq1, celseq2, scscar, nla]"
+    exit
+fi
