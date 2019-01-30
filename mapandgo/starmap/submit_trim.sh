d5412b9ae5c2bcd9c20f7f82eecc0fdc95b50dfe
Anna Alemany <annalemany@gmail.com>
2019-01-30 14:37:39 +0100

    Update submit_trim.sh

--- mapandgo/starmap/submit_trim.sh
+++ mapandgo/starmap/submit_trim.sh
@@ -15,3 +15,6 @@ fi
 input==$1
 
 echo "/hpc/hub_oudenaarden/aalemany/bin/mapandgo2/trim.sh $1" | qsub -cwd -N trim-$1 -o trim-$1.out -e trim-$1.err -m eas -M ${email} -pe threaded ${threads} -l h_rt=${Dt} -l h_vmem=${Dmem}
+
+
+
